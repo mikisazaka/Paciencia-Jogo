66deda6d538068c6245b30b67df6838b0a86b61a
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-04-23 22:01:11 -0300

    feat: geração de cartas do baralho

--- cartas/Baralho.java
+++ cartas/Baralho.java
@@ -1,4 +1,31 @@
 package cartas;
+
+import estruturas_lineares.Pilha;
+
 public class Baralho {
-    
+
+    private Pilha cartas;
+
+    public Baralho() {
+        cartas = new Pilha();
+        gerarBaralho();
+    }
+
+    private void gerarBaralho() {
+
+        String[] naipes = {"Copas", "Ouros", "Paus", "Espadas"};
+        String[] cores = {"Vermelha", "Vermelha", "Preta", "Preta"};
+
+        for (int i = 0; i < naipes.length; i++) {
+            for (int numero = 1; numero <= 13; numero++) {
+                String nome = naipes[i] + numero;
+                Carta c = new Carta(numero, naipes[i], cores[i], false, nome);
+                cartas.push(c);
+            }
+        }
+    }
+
+    public void embaralharCartas() {
+
+    }
 }
--- cartas/Carta.java
+++ cartas/Carta.java
@@ -1,5 +1,57 @@
 package cartas;
 
 public class Carta {
-    
+    private int numero;
+    private String naipe;
+    private String cor;
+    private boolean visibilidade;
+    private String nome;
+
+    public Carta(int numero, String naipe, String cor, boolean visibilidade, String nome) {
+        this.numero = numero;
+        this.naipe = naipe;
+        this.cor = cor;
+        this.visibilidade = visibilidade;
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNaipe() {
+        return naipe;
+    }
+
+    public void setNaipe(String naipe) {
+        this.naipe = naipe;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isVisivel() {
+        return visibilidade;
+    }
+
+    public void setVisibilidade(boolean visibilidade) {
+        this.visibilidade = visibilidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
