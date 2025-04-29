d2e5daa33f4e1000cc576c826fe05b31789c3f3b
giconstancio <nunes.constancio950@gmail.com>
2025-04-29 18:11:50 -0300

    feat: Ajuste do arquivo Main, alterações no menu.

--- Main.java
+++ Main.java
@@ -8,10 +8,11 @@ public class Main {
 
         while (true) {
 
-            System.out.println("Menu principal:");
+            System.out.println("======Menu principal======");
             System.out.println("1 - Embaralhar cartas");
             System.out.println("2 - Iniciar Jogo");
             System.out.println("3 - Sair");
+            System.out.println("");
 
             int opcao = Integer.parseInt(sc.nextLine());
 
@@ -25,8 +26,9 @@ public class Main {
 
                 case 2: {
 
-                    while () {
-                        System.out.println("Menu do jogo:");
+                    boolean jogoFinalizado = false;
+                    while (!jogoFinalizado) {
+                        System.out.println("\n======Menu do jogo======");
                         System.out.println("1 - Movimentar uma carta da fila para pilha");
                         System.out.println("2 - Movimentar da fila para a fila");
                         System.out.println("3 - Movimentar uma carta da fila para uma das listas ligadas");
@@ -34,6 +36,47 @@ public class Main {
                         System.out.println("5 - Movimentar uma carta de uma lista ligada para outra lista ligada");
                         System.out.println("6 - Reiniciar o jogo");
                         System.out.println("7 - Ver estado atual do jogo");
+                        System.out.println("8 - Sair do jogo");
+                        System.out.println("");
+
+                        int opcaoJogo = Integer.parseInt(sc.nextLine());
+
+                        switch (opcaoJogo) {
+                            case 1:
+                            
+                                break;
+
+                            case 2:
+                                
+                                break;
+
+                            case 3:
+                                
+                                break;
+
+                            case 4:
+                                
+                                break;
+
+                            case 5:
+                                
+                                break;
+
+                            case 6:
+                                
+                                break;
+
+                            case 7:
+                                
+                                break;
+                                
+                            case 8:
+                                System.out.println("Saindo do jogo...");
+                                jogoFinalizado = true;
+                                break;
+                            default:
+                                System.out.println("Opção inválida!");
+                        }
                     }
 
                     break;
